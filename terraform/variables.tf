9ec30b00ebcca49a0573acf6bc9f56a5c019d3d1
rrferro <rrferro07@gmail.com>
2025-03-08 17:33:26 -0500

    created terraform folder

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "public_key" {
+  type    = string
+  default = ""
+}
+
+variable "ansible_public_key" {
+  type    = string
+  default = ""  
+}
+
+variable "windows_private_key" {
+  type    = string
+  default = ""
+}
